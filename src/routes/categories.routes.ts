2f342740e85aa344cbd156522d23fa45fd867cb5
Vitor Rubim <vitorrubim1@hotmail.com>
2022-02-07 22:26:45 -0300

    refactor: listagem de categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,21 +1,18 @@
 import { Router } from "express";
 
 // Repositório com os métodos
-import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 // Instâncias
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response)
 });
 
 categoriesRoutes.get("/", (request, response) => {
-  const categories = categoriesRepository.list();
-
-  return response.status(201).json(categories);
+  return listCategoriesController.handle(request, response);
 });
 
 export { categoriesRoutes };
