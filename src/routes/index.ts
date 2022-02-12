f010461ee66ea337b2130b2e9ccd6c5180b0fe40
Vitor Rubim <vitorrubim1@hotmail.com>
2022-02-11 21:55:11 -0300

    refactor: rotas

---
+++ src/routes/index.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+// Routes
+import { categoriesRoutes } from "./categories.routes";
+import { specificationsRoutes } from "./specifications.routes";
+
+const router = Router();
+
+router.use("/categories", categoriesRoutes);
+router.use("/specifications", specificationsRoutes);
+
+export { router };
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,10 @@
 import express from "express";
 
-// Routes
-import { categoriesRoutes } from "./routes/categories.routes";
-import { specificationsRoutes } from "./routes/specifications.routes";
+import { router } from "./routes";
 
 const app = express();
 
 app.use(express.json());
-
-app.use("/categories", categoriesRoutes);
-app.use("/specifications", specificationsRoutes);
+app.use(router);
 
 app.listen(3333, () => console.log("\n\nServer is running! 🏃"));
