e98e0aac8186785a61981a013b207c5573385b33
Vitor Rubim <vitorrubim1@hotmail.com>
2022-02-27 17:18:39 -0300

    feat: add typeorm nas entidade de categoria

--- src/modules/cars/entities/Category.ts
+++ src/modules/cars/entities/Category.ts
@@ -1,11 +1,18 @@
 import { v4 as uuid } from 'uuid';
-import { Entity } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
 
 @Entity('categories')
 class Category {
+  @PrimaryColumn()
   id?: string;
+
+  @Column()
   name: string;
+
+  @Column()
   description: string;
+
+  @CreateDateColumn()
   created_at: Date;
 
   // Quando essa classe é instanciada o id é gerado automaticamente
