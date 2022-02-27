5dc24dd04e23de83a5bd7293cd28fb8dc507f035
Vitor Rubim <vitorrubim1@hotmail.com>
2022-02-27 17:10:26 -0300

    feat: criando a migration de categoria

---
+++ src/database/migrations/1645992062614-CreateCategories.ts
@@ -0,0 +1,25 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateCategories1645992062614 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table(
+        {
+          name: "categories",
+          columns: [
+            { name: "id", type: "uuid", isPrimary: true },
+            { name: "name", type: "varchar" },
+            { name: "description", type: "varchar" },
+            { name: "created_at", type: "timestamp", default: "now()" },
+          ]
+        }
+      )
+    )
+  }
+
+  // Faz o inverso do método "up"
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("categories")
+  }
+}
