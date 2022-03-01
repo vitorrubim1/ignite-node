454e3827941742458fbe8111e5ebff59df73f7e1
Vitor Rubim <vitorrubim1@hotmail.com>
2022-03-01 19:30:06 -0300

    docs: adicionando rotas na documentaçao do swagger

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -3,7 +3,6 @@ import { getRepository, Repository } from "typeorm";
 import { Specification } from "../../entities/Specification";
 
 import { ICreateSpecificationDTO, ISpecificationsRepository } from "../ISpecificationsRepository";
-
 class SpecificationRepository implements ISpecificationsRepository {
   private repository: Repository<Specification>
 
