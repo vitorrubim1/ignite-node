66d9e607366568b58f2ec5e9dab24e71daab2e92
Vitor Rubim <vitorrubim1@hotmail.com>
2022-03-02 20:45:31 -0300

    feat: removendo arquivo de avatar existente

---
+++ src/utils/file.ts
@@ -0,0 +1,13 @@
+import fs from 'fs';
+
+export const deleteFile = async (fileName: string) => {
+  try {
+    // stat: Verifica se o arquivo existe
+    await fs.promises.stat(fileName);
+  } catch {
+    return;
+  }
+
+  // Remove o arquivo
+  await fs.promises.unlink(fileName);
+};
