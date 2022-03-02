b12050c1184160d478726609198b2652c0a0fa56
Vitor Rubim <vitorrubim1@hotmail.com>
2022-03-01 21:04:01 -0300

    feat: criptografando senha e não permitindo cadastro com mesmo email

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from "tsyringe";
+import { hash } from "bcryptjs";
 
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
@@ -16,7 +17,18 @@ class CreateUserUseCase {
     password,
     driver_license
   }: ICreateUserDTO): Promise<void> {
-    await this.usersRepository.create({ name, email, password, driver_license });
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) throw new Error("User already exist");
+
+    const passwordHash = await hash(password, 8);
+
+    await this.usersRepository.create({
+      name,
+      email,
+      password: passwordHash,
+      driver_license
+    });
   }
 };
 
