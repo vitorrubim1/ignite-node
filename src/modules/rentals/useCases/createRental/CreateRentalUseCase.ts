2c869ae445ec79a4f1c5d9a88dfa914d00283b3e
Vitor Rubim <vitorrubim1@hotmail.com>
2022-03-19 12:55:54 -0300

    feat: trabalhando com data com dayjs e adicionando regra de minimo de 24h para locaçao de um carro

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,6 +1,12 @@
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+
 import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "@modules/rentals/infra/typeorm/repositories/IRentalsRepository";
 import { AppError } from "@shared/errors/AppError";
+
+// Pra normalizar as datas, ficarem todas no mesmo fuso horário
+dayjs.extend(utc);
 
 interface IRequest {
   user_id: string;
@@ -16,6 +22,8 @@ class CreateRentalUseCase {
     car_id,
     expected_return_date,
   }: IRequest): Promise<Rental> {
+    const minimumHourForRental = 24;
+
     // Caso o carro já esteja alugado não será possível um criar um novo aluguel
     const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
       car_id,
@@ -30,6 +38,18 @@ class CreateRentalUseCase {
 
     if (rentalOpenToUser)
       throw new AppError("There's a rental in progress for user");
+
+    // O Aluguel deve ter duração mínima de 24h
+    const dateNow = dayjs().utc().local().format();
+    const expectedReturnDateFormat = dayjs(expected_return_date)
+      .utc()
+      .local()
+      .format();
+
+    const compareDates = dayjs(expectedReturnDateFormat).diff(dateNow, "hours");
+
+    if (compareDates < minimumHourForRental)
+      throw new AppError("Invalid return time");
 
     const rental = await this.rentalsRepository.create({
       user_id,
