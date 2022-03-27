a6f4c9b373831fb667c33e8c18fdb285d7081425
Vitor Rubim <vitorrubim1@hotmail.com>
2022-03-27 19:00:29 -0300

    feat: criando caso de uso para recuperaçao de senha

---
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -0,0 +1,39 @@
+import { inject, injectable } from "tsyringe";
+import { v4 as uuid } from "uuid";
+
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
+import { AppError } from "@shared/errors/AppError";
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
+
+@injectable()
+class SendForgotPasswordMailUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository,
+
+    @inject("UsersTokensRepository")
+    private usersTokensRepository: IUsersTokensRepository,
+
+    @inject("DayJsDateProvider")
+    private dateProvider: IDateProvider,
+  ) {}
+
+  async execute(email: string) {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) throw new AppError("User does not exist");
+
+    const token = uuid();
+
+    const expires_date = this.dateProvider.addHours(3);
+
+    await this.usersTokensRepository.create({
+      refresh_token: token,
+      user_id: user.id,
+      expires_date,
+    });
+  }
+}
+
+export { SendForgotPasswordMailUseCase };
