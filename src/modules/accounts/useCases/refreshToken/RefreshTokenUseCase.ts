a58d3a1f120b01e73565ae5655dcb7896ec9f002
Vitor Rubim <vitorrubim1@hotmail.com>
2022-03-28 20:20:46 -0300

    refactor: corrigindo o refresh token

--- src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -13,6 +13,11 @@ interface IPayload {
   email: string;
 }
 
+interface ITokenResponse {
+  token: string;
+  refresh_token: string;
+}
+
 @injectable()
 class RefreshTokenUseCase {
   constructor(
@@ -23,8 +28,13 @@ class RefreshTokenUseCase {
     private dateProvider: IDateProvider,
   ) {}
 
-  async execute(token: string): Promise<string> {
-    const { secret_refresh_token, expires_refresh_token_days } = auth;
+  async execute(token: string): Promise<ITokenResponse> {
+    const {
+      secret_refresh_token,
+      expires_refresh_token_days,
+      secret_token,
+      expires_in_token,
+    } = auth;
 
     // Verificando se o token recebido é valido
     const { email, sub } = verify(token, secret_refresh_token) as IPayload;
@@ -57,7 +67,15 @@ class RefreshTokenUseCase {
       refresh_token,
     });
 
-    return refresh_token;
+    const newToken = sign({}, secret_token, {
+      subject: user_id,
+      expiresIn: expires_in_token,
+    });
+
+    return {
+      refresh_token,
+      token: newToken,
+    };
   }
 }
 
--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -2,7 +2,6 @@ import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 
 import { AppError } from "@shared/errors/AppError";
-import { UsersTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
 import auth from "@config/auth";
 
 interface IPayload {
@@ -16,24 +15,12 @@ async function ensureAuthenticated(
 ) {
   const authHeader = request.headers.authorization;
 
-  const userTokensRepository = new UsersTokensRepository();
-
   if (!authHeader) throw new AppError("Token missing", 401);
 
   const [, token] = authHeader.split(" ");
 
   try {
-    const { sub: user_id } = verify(
-      token,
-      auth.secret_refresh_token,
-    ) as IPayload;
-
-    const user = await userTokensRepository.findByUserIdAndRefreshToken(
-      user_id,
-      token,
-    );
-
-    if (!user) throw new AppError("User does not exist", 401);
+    const { sub: user_id } = verify(token, auth.secret_token) as IPayload;
 
     // Adicionando no Request do express, pra que outros lugares tenham acesso ao id do user logado
     request.user = {
